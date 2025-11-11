d5d2f07289b432f87232e710ee395e5487c4cd81
thishakyawmh <hirunthishakya.wmh25@gmail.com>
2025-11-11 11:29:21 +0530

    ✏️ Day 16 (3)

---
+++ src/components/loaded.jsx
@@ -0,0 +1,7 @@
+export default function Loaded() {
+  return (
+    <div className="w-full h-screen fixed bg-black/45 top-0 left-0 flex justify-center items-center">
+      <div className="w-[100px] h-[100px] border-2 border-t-transparent rounded-full animate-spin border-blue-900"></div>
+    </div>
+  );
+}
--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { BiPlus } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import Loaded from "../../components/loaded";
 
 export default function AdminProductsPage() {
   const [products, setProducts] = useState([]);
@@ -23,110 +24,114 @@ export default function AdminProductsPage() {
   return (
     <div className="w-full min-h-screen flex justify-center p-10 bg-secondary">
       <div className="w-full max-w-7xl overflow-x-auto shadow-lg rounded-xl bg-primary">
-        <table className="min-w-full divide-y divide-gray-200 text-s">
-          <thead className="bg-accent text-white text-xs">
-            <tr>
-              <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide rounded-tl-lg">
-                Image
-              </th>
-              <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
-                Product ID
-              </th>
-              <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
-                Name
-              </th>
-              <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
-                Price
-              </th>
-              <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
-                Labelled Price
-              </th>
-              <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
-                Category
-              </th>
-              <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
-                Brand
-              </th>
-              <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
-                Model
-              </th>
-              <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
-                Stock
-              </th>
-              <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
-                Availability
-              </th>
-              <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide rounded-tr-lg">
-                Actions
-              </th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {products.map((item, idx) => (
-              <tr
-                key={item.productID}
-                className={idx % 2 === 0 ? "bg-secondary" : "bg-primary"}
-              >
-                <td className="px-3 py-2">
-                  <img
-                    src={item.images[0]}
-                    alt={item.name}
-                    className="w-10 h-10 object-cover rounded-md"
-                  />
-                </td>
-                <td className="px-3 py-2 font-medium">{item.productID}</td>
-                <td className="px-3 py-2">{item.name}</td>
-                <td className="px-3 py-2">{item.price}</td>
-                <td className="px-3 py-2">{item.labelledPrice}</td>
-                <td className="px-3 py-2">{item.category}</td>
-                <td className="px-3 py-2">{item.brand}</td>
-                <td className="px-3 py-2">{item.model}</td>
-                <td className="px-3 py-2">{item.stock}</td>
-                <td className="px-3 py-2">
-                  <span
-                    className={`px-2 py-1 rounded-full text-xs font-semibold ${
-                      item.isAvailable
-                        ? "bg-green-100 text-green-800"
-                        : "bg-red-100 text-red-800"
-                    }`}
-                  >
-                    {item.isAvailable ? "In Stock" : "Out of Stock"}
-                  </span>
-                </td>
-                <td className="px-3 py-2">
-                  <button
-                    onClick={() => {
-                      const token = localStorage.getItem("token");
-                      const url =
-                        import.meta.env.VITE_BACKEND_URL +
-                        "/products/" +
-                        item.productID;
-
-                      axios
-                        .delete(url, {
-                          headers: {
-                            Authorization: `Bearer ${token}`,
-                          },
-                        })
-                        .then(() => {
-                          toast.success("Product deleted successfully");
-                          setLoaded(false);
-                        })
-                        .catch((err) => {
-                          toast.error("Failed to delete product");
-                          console.error("Delete error:", err);
-                        });
-                    }}
-                    className="w-20 bg-red-600 text-white font-semibold py-1 rounded-lg hover:bg-red-700 transition duration-200 cursor-pointer"
-                  >
-                    Delete
-                  </button>
-                </td>
+        {loaded ? (
+          <table className="min-w-full divide-y divide-gray-200 text-s">
+            <thead className="bg-accent text-white text-xs">
+              <tr>
+                <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide rounded-tl-lg">
+                  Image
+                </th>
+                <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
+                  Product ID
+                </th>
+                <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
+                  Name
+                </th>
+                <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
+                  Price
+                </th>
+                <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
+                  Labelled Price
+                </th>
+                <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
+                  Category
+                </th>
+                <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
+                  Brand
+                </th>
+                <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
+                  Model
+                </th>
+                <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
+                  Stock
+                </th>
+                <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide">
+                  Availability
+                </th>
+                <th className="px-3 py-2 text-left font-semibold uppercase tracking-wide rounded-tr-lg">
+                  Actions
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+
+            <tbody>
+              {products.map((item, idx) => (
+                <tr
+                  key={item.productID}
+                  className={idx % 2 === 0 ? "bg-secondary" : "bg-primary"}
+                >
+                  <td className="px-3 py-2">
+                    <img
+                      src={item.images[0]}
+                      alt={item.name}
+                      className="w-10 h-10 object-cover rounded-md"
+                    />
+                  </td>
+                  <td className="px-3 py-2 font-medium">{item.productID}</td>
+                  <td className="px-3 py-2">{item.name}</td>
+                  <td className="px-3 py-2">{item.price}</td>
+                  <td className="px-3 py-2">{item.labelledPrice}</td>
+                  <td className="px-3 py-2">{item.category}</td>
+                  <td className="px-3 py-2">{item.brand}</td>
+                  <td className="px-3 py-2">{item.model}</td>
+                  <td className="px-3 py-2">{item.stock}</td>
+                  <td className="px-3 py-2">
+                    <span
+                      className={`px-2 py-1 rounded-full text-xs font-semibold ${
+                        item.isAvailable
+                          ? "bg-green-100 text-green-800"
+                          : "bg-red-100 text-red-800"
+                      }`}
+                    >
+                      {item.isAvailable ? "In Stock" : "Out of Stock"}
+                    </span>
+                  </td>
+                  <td className="px-3 py-2">
+                    <button
+                      onClick={() => {
+                        const token = localStorage.getItem("token");
+                        const url =
+                          import.meta.env.VITE_BACKEND_URL +
+                          "/products/" +
+                          item.productID;
+
+                        axios
+                          .delete(url, {
+                            headers: {
+                              Authorization: `Bearer ${token}`,
+                            },
+                          })
+                          .then(() => {
+                            toast.success("Product deleted successfully");
+                            setLoaded(false);
+                          })
+                          .catch((err) => {
+                            toast.error("Failed to delete product");
+                            console.error("Delete error:", err);
+                          });
+                      }}
+                      className="w-20 bg-red-600 text-white font-semibold py-1 rounded-lg hover:bg-red-700 transition duration-200 cursor-pointer"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <Loaded />
+        )}
       </div>
 
       <Link
